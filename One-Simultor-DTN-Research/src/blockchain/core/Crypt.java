c6186850327dac83754be8d80307373d7c601176
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-07-02 00:49:18 +0530

    Crypt Added

---
+++ One-Simultor-DTN-Research/src/blockchain/core/Crypt.java
@@ -0,0 +1,38 @@
+package blockchain.core;
+
+import java.security.MessageDigest;
+
+public class Crypt { 
+ 
+   public static String sha256(String input) 
+   { 
+       try { 
+           MessageDigest sha 
+               = MessageDigest 
+                     .getInstance( 
+                         "SHA-256"); 
+           int i = 0; 
+ 
+           byte[] hash 
+               = sha.digest( 
+                   input.getBytes("UTF-8")); 
+ 
+           StringBuffer hexHash = new StringBuffer(); 
+ 
+           while (i < hash.length) { 
+               String hex 
+                   = Integer.toHexString( 
+                       0xff & hash[i]); 
+               if (hex.length() == 1) 
+                   hexHash.append('0'); 
+               hexHash.append(hex); 
+               i++; 
+           } 
+ 
+           return hexHash.toString(); 
+       } 
+       catch (Exception e) { 
+           throw new RuntimeException(e); 
+       } 
+   } 
+} 
