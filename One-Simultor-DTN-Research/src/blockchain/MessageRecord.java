33a4dfc7f5e3d4e53d943e36b18c58b055bf5215
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-07-02 00:02:48 +0530

    msg record

---
+++ One-Simultor-DTN-Research/src/blockchain/MessageRecord.java
@@ -0,0 +1,35 @@
+package blockchain;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MessageRecord {
+
+
+	public String from;
+	
+	public String to;
+	
+	public boolean recieved;
+	
+	public Set<String> carry=new TreeSet<>();
+	
+	public MessageRecord() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "MessageRecord [from=" + from + ", to=" + to + ", recieved=" + recieved + ", carry=" + carry + "]\n";
+	}
+
+	public MessageRecord(String from, String to, boolean recieved) {
+		super();
+		this.from = from;
+		this.to = to;
+		this.recieved = recieved;
+	}
+	
+	
+	
+}
