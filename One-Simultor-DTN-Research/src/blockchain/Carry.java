6b22c0f140738f5db2d65596cf0727d2842a82fe
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-07-02 00:02:20 +0530

    carry

---
+++ One-Simultor-DTN-Research/src/blockchain/Carry.java
@@ -0,0 +1,42 @@
+/*
+ * author @Soumyadip
+ */
+package blockchain;
+
+/**
+ * A generic key-value tuple 
+ */
+public class Carry<MsgId,Time>  {
+	private MsgId key;
+	private Time value;
+	/**
+	 * Creates a new tuple.
+	 * @param key The key of the tuple
+	 * @param value The value of the tuple
+	 */
+	public Carry(MsgId key, Time value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+	public Carry() {
+		super();
+	}
+	public MsgId getKey() {
+		return key;
+	}
+	public void setKey(MsgId key) {
+		this.key = key;
+	}
+	public Time getValue() {
+		return value;
+	}
+	public void setValue(Time value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "Carry [key=" + key + ", value=" + value + "]\n";
+	}
+	
+}
